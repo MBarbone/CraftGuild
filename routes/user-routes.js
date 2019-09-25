fe49760d31f1ee7b2d2c8cd6a2c949aa42a03782
Mike <michael.barbone@gmail.com>
2019-09-24 22:14:44 -0400

    typo fix in jwt

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const router = express.Router();
-const jwt = "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
